a69f46ef26e77f3943823dd186ea61bad76af707
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-04 23:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const input = document.getElementById("fname");
-        
-input.addEventListener("blur",()=>{
-            input.value=input.value.toUppercase();
-        });
+    function capitalizeName() {
+      input.value = input.value.toUpperCase();
+    }
+    input.addEventListener("blur", capitalizeName);
