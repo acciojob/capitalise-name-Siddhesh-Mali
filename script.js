84838177a29a049c9b96cd1cc867649adb7b7903
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-04 21:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const input = document.getElementById("fname");
+        
+input.addEventListener("blur",()=>{
+            input.value=input.value.toUppercase();
+        });
